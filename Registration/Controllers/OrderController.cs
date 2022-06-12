4dfb9d8700a96c0a805f454423a90cfe5cef60b9
diogo-soares <diogosoaresk8@hotmail.com>
2022-06-12 16:36:43 -0300

    including controller and models

---
+++ Registration/Controllers/OrderController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared;
+
+namespace Registration.Controllers
+{
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost]
+        public async Task PostAsync([FromBody] Order order)
+        {
+            order.Id = Guid.NewGuid().ToString();
+            order.CreationDate = DateTime.Now;
+
+            await order.SaveAsync();
+
+            Console.WriteLine($"Order saved successfully: id {order.Id}");
+        }
+    }
+}
---
+++ Shared/Amazon.cs
@@ -0,0 +1,17 @@
+﻿using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
+namespace Shared
+{
+    public static class Amazon
+    {
+        public static async Task SaveAsync(this Order order)
+        {
+            var client = new AmazonDynamoDBClient(RegionEndpoint.SAEast1);
+            var context = new DynamoDBContext(client);
+
+            await context.SaveAsync(order);
+        }
+    }
+}
